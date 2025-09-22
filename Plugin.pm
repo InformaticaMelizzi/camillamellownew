dd4f47d62d4fbdc26a9d573efb918d9a6dc85ce0
Dave Mellow <davemellow@MacBook-Air-di-Mara.local>
2025-09-22 14:13:15 +0200

    PCM-BASE: Mellow-Dsp-FIR v0.2.0-PCM-BASE with SHA ba709ffc173fdfa14e67bc8525a55bb15bbb1579

--- Plugin.pm
+++ Plugin.pm
@@ -6,6 +6,7 @@ use warnings;
 use Slim::Utils::Log;
 use Slim::Utils::Prefs;
 use Slim::Utils::Strings;
+use Slim::Control::Request;
 
 my $log = logger('plugin.mellowdspfir');
 my $prefs = preferences('plugin.mellowdspfir');
@@ -16,38 +17,17 @@ sub initPlugin {
     $log->info("Mellow-Dsp-FIR plugin v0.2.0-PCM initialized");
     
     # Load settings modules
-    Plugins::MellowDspFIR::Settings->new();
-    Plugins::MellowDspFIR::PlayerSettings->new();
+    eval {
+        require Plugins::MellowDspFIR::Settings;
+        Plugins::MellowDspFIR::Settings->new();
+    };
     
-    # Subscribe to player events
-    Slim::Control::Request::subscribe(\&playerChangeCallback, [['client'], ['new', 'reconnect', 'disconnect']]);
+    if ($@) {
+        $log->error("Failed to load settings: $@");
+        return 0;
+    }
     
     return 1;
-}
-
-sub playerChangeCallback {
-    my ($request) = @_;
-    my $client = $request->client();
-    
-    return unless $client;
-    
-    my $enabled = $prefs->client($client)->get('enabled');
-    
-    if ($enabled) {
-        $log->info("Mellow-Dsp-FIR PCM processing enabled for player: " . $client->name());
-        # Initialize CamillaDSP pipeline for PCM
-        initCamillaDSP($client);
-    }
-}
-
-sub initCamillaDSP {
-    my ($client) = @_;
-    
-    $log->info("Initializing CamillaDSP for PCM pass-through");
-    $log->info("Target: 768kHz PCM, 32-bit processing → 24-bit output with E-weighted dithering");
-    
-    # CamillaDSP initialization for PCM pipeline
-    # Direct PCM processing without WAV wrapper
 }
 
 1;
--- Settings.pm
+++ Settings.pm
@@ -5,6 +5,7 @@ use warnings;
 
 use Slim::Utils::Log;
 use Slim::Utils::Prefs;
+use Slim::Web::Pages;
 
 my $log = logger('plugin.mellowdspfir');
 my $prefs = preferences('plugin.mellowdspfir');
@@ -12,21 +13,19 @@ my $prefs = preferences('plugin.mellowdspfir');
 sub new {
     my ($class) = @_;
     
-    # Add web pages
-    Slim::Web::Pages->addPageFunction(
-        'plugins/Mellow-Dsp-FIR/settings/basic.html',
-        \&basicSettings
-    );
+    $log->info("Loading MellowDspFIR Settings");
     
-    Slim::Web::Pages->addPageFunction(
-        'plugins/Mellow-Dsp-FIR/settings/fir.html',
-        \&firSettings
-    );
+    # Add web page
+    eval {
+        Slim::Web::Pages->addPageFunction(
+            'plugins/Mellow-Dsp-FIR/settings/basic.html',
+            \&basicSettings
+        );
+    };
     
-    Slim::Web::Pages->addPageFunction(
-        'plugins/Mellow-Dsp-FIR/settings/advanced.html',
-        \&advancedSettings
-    );
+    if ($@) {
+        $log->error("Failed to add web page: $@");
+    }
     
     return $class;
 }
@@ -37,61 +36,13 @@ sub basicSettings {
     if ($params->{saveSettings}) {
         $prefs->set('enabled', $params->{enabled} ? 1 : 0);
         $prefs->set('pcm_sample_rate', $params->{pcm_sample_rate} || '768000');
-        $prefs->set('pcm_bit_depth', $params->{pcm_bit_depth} || '32');
-        $prefs->set('pcm_dither', $params->{pcm_dither} || 'e_weighted');
     }
     
     $params->{enabled} = $prefs->get('enabled');
     $params->{pcm_sample_rate} = $prefs->get('pcm_sample_rate');
-    $params->{pcm_bit_depth} = $prefs->get('pcm_bit_depth');
-    $params->{pcm_dither} = $prefs->get('pcm_dither');
     
     return Slim::Web::HTTP::filltemplatefile(
         'plugins/Mellow-Dsp-FIR/settings/basic.html',
-        $params
-    );
-}
-
-sub firSettings {
-    my ($client, $params, $callback, @args) = @_;
-    
-    if ($params->{saveSettings}) {
-        $prefs->set('fir_enabled', $params->{fir_enabled} ? 1 : 0);
-        $prefs->set('fir_file_left', $params->{fir_file_left} || '');
-        $prefs->set('fir_file_right', $params->{fir_file_right} || '');
-        $prefs->set('fir_gain', $params->{fir_gain} || '0');
-        $prefs->set('pcm_upsampling', $params->{pcm_upsampling} || '16x');
-    }
-    
-    $params->{fir_enabled} = $prefs->get('fir_enabled');
-    $params->{fir_file_left} = $prefs->get('fir_file_left');
-    $params->{fir_file_right} = $prefs->get('fir_file_right');
-    $params->{fir_gain} = $prefs->get('fir_gain');
-    $params->{pcm_upsampling} = $prefs->get('pcm_upsampling');
-    
-    return Slim::Web::HTTP::filltemplatefile(
-        'plugins/Mellow-Dsp-FIR/settings/fir.html',
-        $params
-    );
-}
-
-sub advancedSettings {
-    my ($client, $params, $callback, @args) = @_;
-    
-    if ($params->{saveSettings}) {
-        $prefs->set('camilla_pcm_config', $params->{camilla_pcm_config} || '');
-        $prefs->set('pcm_buffer_size', $params->{pcm_buffer_size} || '2048');
-        $prefs->set('pcm_phase_response', $params->{pcm_phase_response} || 'linear');
-        $prefs->set('pcm_target_depth', $params->{pcm_target_depth} || '24');
-    }
-    
-    $params->{camilla_pcm_config} = $prefs->get('camilla_pcm_config');
-    $params->{pcm_buffer_size} = $prefs->get('pcm_buffer_size');
-    $params->{pcm_phase_response} = $prefs->get('pcm_phase_response');
-    $params->{pcm_target_depth} = $prefs->get('pcm_target_depth');
-    
-    return Slim::Web::HTTP::filltemplatefile(
-        'plugins/Mellow-Dsp-FIR/settings/advanced.html',
         $params
     );
 }
