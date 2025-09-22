d3381a065156e205a61e39005c846fe319da1e9f
Dave Mellow <davemellow@MacBook-Air-di-Mara.local>
2025-09-22 12:31:25 +0200

    FEAT: Aggiunto upsampling completo 44.1-768kHz, phase response, output depth, dithering e target precision

--- CamillaMellow/PlayerSettings.pm
+++ CamillaMellow/PlayerSettings.pm
@@ -12,7 +12,6 @@ my $prefs = preferences('plugin.camillamellow');
 sub new {
     my ($class) = @_;
     
-    # Registra le impostazioni del player - COPIA ESATTA C-3PO
     Slim::Web::Pages->addPageFunction(
         'plugins/CamillaMellow/settings/basic.html',
         \&settingsPage
@@ -26,11 +25,21 @@ sub settingsPage {
     
     if ($params->{saveSettings}) {
         $prefs->client($client)->set('enabled', $params->{enabled} ? 1 : 0);
+        $prefs->client($client)->set('upsample_rate', $params->{upsample_rate} || '44100');
+        $prefs->client($client)->set('phase_response', $params->{phase_response} || 'linear');
+        $prefs->client($client)->set('output_depth', $params->{output_depth} || '24');
+        $prefs->client($client)->set('dithering', $params->{dithering} || 'none');
+        $prefs->client($client)->set('target_precision', $params->{target_precision} || '24');
         $prefs->client($client)->set('fir_file_left', $params->{fir_file_left} || '');
         $prefs->client($client)->set('fir_file_right', $params->{fir_file_right} || '');
     }
     
     $params->{enabled} = $prefs->client($client)->get('enabled');
+    $params->{upsample_rate} = $prefs->client($client)->get('upsample_rate');
+    $params->{phase_response} = $prefs->client($client)->get('phase_response');
+    $params->{output_depth} = $prefs->client($client)->get('output_depth');
+    $params->{dithering} = $prefs->client($client)->get('dithering');
+    $params->{target_precision} = $prefs->client($client)->get('target_precision');
     $params->{fir_file_left} = $prefs->client($client)->get('fir_file_left');
     $params->{fir_file_right} = $prefs->client($client)->get('fir_file_right');
     $params->{player} = $client->id();
